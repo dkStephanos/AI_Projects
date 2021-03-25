911d96ef7b646ec44b330427c62586585b146cd9
dkStephanos <stephanos@goldmail.etsu.edu>
2021-03-25 16:57:18 -0400

    Working on project 3

--- Project 3/project3/KnowledgeBase.py
+++ Project 3/project3/KnowledgeBase.py
@@ -13,8 +13,47 @@ class KnowledgeBase:
                                     in solving the problem.
             self.the_character - The random character to interrogate
         """
-        self.characters = []            # A list of all characters, initially (which are dictionaries)
-        self.characteristics = {}       # A dictionary of all known characteristics
+        self.characters = ['ALEX', 
+                           'ALFRED', 
+                           'ANITA', 
+                           'ANNE', 
+                           'BERNARD', 
+                           'BILL', 
+                           'CHARLES', 
+                           'CLAIRE', 
+                           'DAVID', 
+                           'ERIC', 
+                           'FRANS', 
+                           'GEORGE', 
+                           'HERMAN', 
+                           'JOE', 
+                           'MARIA', 
+                           'MAX', 
+                           'PAUL', 
+                           'PETER', 
+                           'PHILIP', 
+                           'RICHARD', 
+                           'ROBERT', 
+                           'SAM', 
+                           'SUSAN', 
+                           'TOM', ]            # A list of all characters, initially (which are dictionaries)
+        self.characteristics = {
+            'IS_MALE': ['ALEX', 'ALFRED', 'BERNARD', 'BILL', 'CHARLES', 'DAVID', 'ERIC', 'FRANS', 'GEORGE', 'HERMAN', 'JOE', 'MAX', 'PAUL', 'PETER', 'PHILIP', 'RICHARD', 'ROBERT', 'SAM', 'TOM',],
+            'IS_FEMALE': ['ANITA', 'ANNE', 'CLAIRE', 'MARIA', 'SUSAN'],
+            'BLACK_HAIR': ['ALEX', 'ANNE', 'MAX', 'PHILIP', 'TOM'],
+            'RED_HAIR': ['ALFRED', 'BILL', 'CLAIRE', 'FRANS', 'HERMAN', 'ROBERT'],
+            'WHITE_HAIR': ['ANITA', 'GEORGE', 'PAUL', 'PETER', 'SAM'],
+            'BROWN_HAIR': ['BERNARD', 'MARIA', 'RICHARD'],
+            'BLONDE_HAIR': ['CHARLES', 'DAVID', 'ERIC', 'JOE', 'SUSAN'],
+            'BALD': ['BILL', 'HERMAN', 'RICHARD', 'SAM', 'TOM'],
+            'HAT': ['BERNARD', 'CLAIRE', 'ERIC', 'GEORGE', 'MARIA'],
+            'BLUE_EYES': ['ALFRED', 'ANITA', 'PETER', 'ROBERT', 'TOM'],
+            'BROWN_EYES': ['ALEX', 'ANNE', 'BERNARD', 'BILL', 'CHARLES', 'CLAIRE', 'DAVID', 'ERIC', 'FRANS', 'GEORGE', 'HERMAN', 'JOE', 'MARIA', 'MAX', 'PAUL', 'PHILIP', 'RICHARD', 'SAM', 'SUSAN', ],
+            'MUSTACHE': ['ALEX', 'ALFRED', 'CHARLES', 'MAX', 'RICHARD'],
+            'BEARD': ['BILL', 'DAVID', 'PHILIP', 'RICHARD'],
+            'GLASSES': ['CLAIRE', 'JOE', 'PAUL', 'SAM', 'TOM'],
+            'EARINGS': ['ANNE', 'MARIA', 'SUSAN']
+        }       # A dictionary of all known characteristics
 
         ##-- Read the characters and characteristics
         self.read_characters()
