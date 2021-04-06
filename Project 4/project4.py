2ebcffdc5ee03c2236d2369cc59b830fd2b495c0
dkStephanos <stephanos@goldmail.etsu.edu>
2021-04-06 18:20:26 -0400

    Set up rest of clusters, got mean quality scores per cluster

--- Project 4/project4.py
+++ Project 4/project4.py
@@ -5,18 +5,24 @@ from sklearn.cluster import KMeans
 
 white_df = pd.read_csv('winequality-white.csv')
 target_white = white_df['quality']
-white_df = white_df.drop(columns=['quality'])
+white_data = white_df.drop(columns=['quality'])
 
-red_df = pd.read_csv('winequality-red.csv')
-target_red = red_df['quality']
-red_df = red_df.drop(columns=['quality'])
-
-white_clusters = KMeans(n_clusters=11).fit(white_df)
+white_clusters = KMeans(n_clusters=11).fit(white_data)
 white_labels = white_clusters.labels_
-red_clusters = KMeans(n_clusters=11).fit_predict(red_df)
 
 clusters = {}
 for i in range(11):
-    clusters[i] = white_df.values[white_labels == i, :]    # Subset of the datapoints that have been assigned to the cluster i
+    clusters[i] = pd.DataFrame(white_df.values[white_labels == i, :])    # Subset of the datapoints that have been assigned to the cluster i
+    print(clusters[i][11].mean())
 
-print(clusters)
+red_df = pd.read_csv('winequality-red.csv')
+target_red = red_df['quality']
+red_data = red_df.drop(columns=['quality'])
+
+red_clusters = KMeans(n_clusters=11).fit(red_data)
+red_labels = red_clusters.labels_
+
+clusters = {}
+for i in range(11):
+    clusters[i] = pd.DataFrame(red_df.values[red_labels == i, :])    # Subset of the datapoints that have been assigned to the cluster i
+    print(clusters[i][11].mean())
